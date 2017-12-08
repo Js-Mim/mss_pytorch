00891df7854a8c413ac666d4c1ac0e429d790bcd
S.I. Mimilakis <jsmimilak@gmail.com>
2017-12-08 14:36:20 +0100

    Added MSE criterion, such that it can be used for recurrent inference.

--- losses/loss_functions.py
+++ losses/loss_functions.py
@@ -11,4 +11,8 @@ def kullback_leibler(x, x_hat):
     rec = torch.sum(x * (torch.log(x + 1e-6) - torch.log(x_hat + 1e-6)) + (x_hat - x), dim=-1)
     return torch.mean(rec)
 
+
+def mse(x, x_hat):
+    return torch.mean(torch.pow(x - x_hat, 2.))
+
 # EOF
